21e3320d41d940cc597b1338e9b40fe3c486d57d
Todor <toshopz26@gmail.com>
2021-08-10 23:22:37 +0300

    Adding some basics exercised

---
+++ Programming-Basics/First_Steps_In_Coding/Pet_Shop.py
@@ -0,0 +1,6 @@
+dogs_count = int(input())
+other_animals = int(input())
+dog_food_price = 2.50
+other_animals_food = 4
+total_sum = dogs_count*dog_food_price + other_animals*other_animals_food
+print(f"{total_sum} lv.")
---
+++ Programming-Basics/First_Steps_In_Coding/Yard_Greening.py
@@ -0,0 +1,6 @@
+yards = float(input())
+price = yards * 7.61
+discount = 0.18*price
+final_price = price - discount
+print(f"The final price is: {final_price} lv.")
+print(f"The discount is {discount} lv")
